1d2e9191612353d3dd586666133c47d9e31ef328
bob <stevolisisjoseph@gmail.com>
2022-08-29 23:27:47 +0100

    Navbar Changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,11 +6,11 @@ export default function Navbar(){
         <>
         <div className="navbar">
             <div className="subNav1">
-            <div><Link href='#'>Home</Link></div>
-            <div><Link href='#'>About Us</Link></div>
-            <div><Link href='#'>Our Services</Link></div>
-            <div><Link href='#'>Learn More</Link></div>
-            <div><Link href='#'>Contact Us</Link></div>
+            <div><Link href='/'>Home</Link></div>
+            <div><Link href='https://ototech22.github.io/OTOTECH-website/about.html'>About Us</Link></div>
+            <div><Link href='https://ototech22.github.io/OTOTECH-website/#service'>Our Services</Link></div>
+            <div><Link href='https://ototech22.github.io/OTOTECH-website/'>Learn More</Link></div>
+            <div><Link href='https://ototech22.github.io/OTOTECH-website/contact.html'>Contact Us</Link></div>
             </div>
             <div className="subNav2">
                 <Link href='#'>Lets Build your Project</Link>
