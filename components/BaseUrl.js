5fc0d70ae74496d09482ce1bdfc561f45d72f40d
Steven Joseph <stevolisisjoseph@gmail.com>
2023-02-01 22:48:02 +0100

    ototechBlog api fixes9

--- components/BaseUrl.js
+++ components/BaseUrl.js
@@ -3,7 +3,7 @@ const phpUrl2='http://localhost';
 // const phpUrl='https://ototech-blog.vercel.app/';
 const phpUrl='https://2345679090.000webhostapp.com';
 
-const baseUrl='http://localhost:3000';
+const baseUrl='https://ototech-blog.vercel.app';
 const baseUrl3='https://main--reliable-tapioca-719734.netlify.app';
 // const baseUrl='https://ototechblog.vercel.app';
 
