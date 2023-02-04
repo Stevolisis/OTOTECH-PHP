7964004f21a115e4eaa6558af3a1aee5dbe831b7
Steven Joseph <stevolisisjoseph@gmail.com>
2023-02-04 22:33:13 +0100

    cookie fixes

--- components/BaseUrl.js
+++ components/BaseUrl.js
@@ -1,10 +1,11 @@
 
-const phpUrl2='http://localhost';
+const phpUrl2='http://localhost/ototech_api/ototech_api';
 // const phpUrl='https://ototech-blog.vercel.app/';
 const phpUrl='https://2345679090.000webhostapp.com';
 
 const baseUrl='https://ototech-blog.vercel.app';
 const baseUrl3='https://main--reliable-tapioca-719734.netlify.app';
 // const baseUrl='https://ototechblog.vercel.app';
+const baseUrl2='http://localhost:3000';
 
 module.exports={baseUrl,phpUrl}
--- middleware.js
+++ middleware.js
@@ -11,11 +11,11 @@ export default async function middleware(req) {
         return NextResponse.error();
       }
 
-      if(req.cookies.get('adminPass')== undefined){
+      // if(req.cookies.get('adminPass')== undefined){
 
-        return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`); 
+      //   return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`); 
 
-      }else{
+      // }else{
 
         const res=await fetch(`${phpUrl}/authentication/admin-auth.php?cookie=${cookie}`)
       //  const res=await fetch(`${baseUrl}/api/authentication/adminAuth?cookie=${cookie}`)
@@ -25,7 +25,7 @@ export default async function middleware(req) {
         return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`);
       }
 
-      }
+      // }
 
     }
 
