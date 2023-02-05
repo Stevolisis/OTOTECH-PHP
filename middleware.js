e68af529a1a9cf4325638599e2359d158474802c
Steven Joseph <stevolisisjoseph@gmail.com>
2023-02-05 15:39:13 +0100

    cookie fixes20

--- middleware.js
+++ middleware.js
@@ -6,31 +6,31 @@ export default async function middleware(req) {
   let next=req.url.split(baseUrl)[1];
 
 
-    if (req.url.includes('/admin')) {
-      if (req.method == "HEAD") {
-        return NextResponse.error();
-      }
+  if (req.url.includes('/admin')) {
+    if (req.method == "HEAD") {
+      return NextResponse.error();
+    }
 
-      // if(req.cookies.get('adminPass2')== undefined){
+    // if(req.cookies.get('adminPass2')== undefined){
 
-      //   return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`); 
+    //   return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`); 
 
-      // }else{
+    // }else{
 
-        const res=await fetch(`${phpUrl}/authentication/admin-auth.php`,{method: 'POST',credentials:'include'})
-      //  const res=await fetch(`${baseUrl}/api/authentication/adminAuth?cookie=${cookie}`)
-      //   if(res.status==404){
-      //     return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`);
-      //   }else if(res.status==200){
-      //   return NextResponse.next();
-      // }else{
-      //   return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`);
-      // }
-      const response = NextResponse.redirect(`${baseUrl}/login?next=${next}&from=adminRoutes`)
-      response.cookies.set("myCookie", res.status);
-      return response;
+      const res=await fetch(`${phpUrl}/authentication/admin-auth.php`,{method: 'POST',credentials:'include'})
+    //  const res=await fetch(`${baseUrl}/api/authentication/adminAuth?cookie=${cookie}`)
+      if(res.status==404){
+        return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`);
+      }else if(res.status==200){
+      return NextResponse.next();
+    }else{
+      return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`);
+    }
+    // const response = NextResponse.redirect(`${baseUrl}/login?next=${next}&from=adminRoutes`)
+    // response.cookies.set("myCookie", res.status);
+    // return response;
 
-      // }
+    // }
 
     }
 
