b8a5f4e3fe228ffc92a2a6ca591252711bd27f80
Steven Joseph <stevolisisjoseph@gmail.com>
2023-02-12 22:12:52 +0100

    cookies fixes21

--- middleware.js
+++ middleware.js
@@ -2,35 +2,31 @@ import { NextResponse } from "next/server"
 import { baseUrl, phpUrl } from "./components/BaseUrl"
 
 export default async function middleware(req) {
-  // let cookie=req.cookies.get('adminPass2');
+  let cookie=req.cookies.get('adminPass2');
   let next=req.url.split(baseUrl)[1];
 
 
-  if (req.url.includes('/admin')) {
-    if (req.method == "HEAD") {
-      return NextResponse.error();
-    }
+    if (req.url.includes('/admin')) {
+      if (req.method == "HEAD") {
+        return NextResponse.error();
+      }
 
-    // if(req.cookies.get('adminPass2')== undefined){
-
-    //   return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`); 
-
-    // }else{
-
-      const res=await fetch(`${phpUrl}/authentication/admin-auth.php`,{method: 'POST',credentials:'include'})
-    //  const res=await fetch(`${baseUrl}/api/authentication/adminAuth?cookie=${cookie}`)
-      if(res.status==404){
+      if (!cookie) {
         return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`);
-      }else if(res.status==200){
-      return NextResponse.next();
-    }else{
-      return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`);
-    }
-    // const response = NextResponse.redirect(`${baseUrl}/login?next=${next}&from=adminRoutes`)
-    // response.cookies.set("myCookie", res.status);
-    // return response;
-
-    // }
+      } else {
+        try {
+          const res=await fetch(`${phpUrl}/authentication/admin-auth.php`,{method: 'POST',credentials:'include'})
+          if (res.status !== 404) {
+            return NextResponse.next();
+          } else {
+            return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`);
+          }
+        } catch (error) {
+          console.error(error);
+          return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`);
+        }
+      }
+      
 
     }
 
