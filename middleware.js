b4691d1b729f53a9d83b1ff00aa160a978c8b063
Steven Joseph <stevolisisjoseph@gmail.com>
2023-02-12 23:30:10 +0100

    cookies fixes23

--- middleware.js
+++ middleware.js
@@ -1,8 +1,9 @@
+
 import { NextResponse } from "next/server"
 import { baseUrl, phpUrl } from "./components/BaseUrl"
 
 export default async function middleware(req) {
-  let cookie=req.cookies.get('adminPass2');
+  let cookie=req.cookies.get('adminPass');
   let next=req.url.split(baseUrl)[1];
 
 
@@ -15,7 +16,7 @@ export default async function middleware(req) {
         return NextResponse.rewrite(`${baseUrl}/login?next=${next}&from=adminRoutes`);
       } else {
         try {
-          const res=await fetch(`${phpUrl}/authentication/admin-auth.php?cookie=${cookie.value}`)
+          const res=await fetch(`${phpUrl}/authentication/admin-auth.php?cookie=${cookie}`)
           if (res.status !== 404) {
             return NextResponse.next();
           } else {
